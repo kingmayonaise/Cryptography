9b918b33b3cc47e284b9413fa94535065a7c1d1b
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2016-10-19 18:20:02 -0400

    Updated from Brython Server: 10/19/2016 6:20:01 PM

--- cryptography.py
+++ cryptography.py
@@ -10,20 +10,22 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-from operator import add
+from operator import add, sub
 loop=True
-char=[]
-keyChar=[]
 encrypted=""
+decrypted=""
 while loop==True:
     enterletter=input("Enter e to encrypt,d to decrypt, or q to quit: ")
     if enterletter=='e':
+        char=[]
+        keyChar=[]
         #message=input("Message: ")
         message="Two plus two = Five"
         #key=input("Key: ")
         key="Lorem ipsum"
         while len(key)<len(message):
             key+=key[:min(len(key),len(message)-len(key))]
+        print(key)
         for i in message:
             char.append(associations.find(i))
         for n in key:
@@ -35,12 +37,35 @@ while loop==True:
             else:
                 encrypted+=associations[x]
         print(encrypted)
-        loop=False
+        #loop=False
     elif enterletter=='d':
-        print('Decrypt')
+        char=[]
+        keyChar=[]
+        #message=input("Message: ")
+        message="+KF;B(CH=NIZ};R\Dt"
+        #key=input("Key: ")
+        key="Lorem ipsum"
+        while len(key)<len(message):
+            key+=key[:min(len(key),len(message)-len(key))]
+        print(key)
+        for i in message:
+            char.append(associations.find(i))
+        for n in key:
+            keyChar.append(associations.find(n))
+        keyandChar=list(map(sub, char, keyChar))
+        print(len(char))
+        print(len(keyChar))
+        print(keyandChar)
+        for x in keyandChar:
+            if x<len(associations):
+                x+=len(associations)
+            else:
+                decrypted+=associations[x]
+        print(decrypted)
+        print('Decrypt works')
         loop=False
     elif enterletter=='q':
-        print('Goodbye')
+        print('Goodbye!')
         loop=False
     else:
         print('Did not understand command, try again.')
